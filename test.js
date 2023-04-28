bc34332651a115f5778738680f0dd64281e8733d
rs183c56 <b.ricx14@gmail.com>
2023-04-29 07:41:32 +0800

    Adding alert

--- test.js
+++ test.js
@@ -4,61 +4,45 @@ function addQuantity() {
     document.getElementById('quantity').innerHTML = quantity;
     document.getElementById('quantity1').innerHTML = quantity;
     console.log(quantity);
-    var price = 1000;
+    // var price = 1000;
 
     if (quantity == 1) {
         document.getElementById('subtotal').innerHTML = quantity * 1000;
         document.getElementById('total').innerHTML = (quantity * 1000) + 50;
         document.getElementById('total2').innerHTML = (quantity * 1000) + 50;
     }
-
-    else if (quantity == 2) {
+    else if (quantity <= 10) {
         document.getElementById('subtotal').innerHTML = quantity * 1000;
         document.getElementById('total').innerHTML = (quantity * 1000) + 50;
         document.getElementById('total2').innerHTML = (quantity * 1000) + 50;
     }
-    else if (quantity == 3) {
-        document.getElementById('subtotal').innerHTML = quantity * 1000;
-        document.getElementById('total').innerHTML = (quantity * 1000) + 50;
-    }
-    else if (quantity == 4) {
-        document.getElementById('subtotal').innerHTML = quantity * 1000;
-        document.getElementById('total').innerHTML = (quantity * 1000) + 50;
-    }
-    else if (quantity == 5) {
-        document.getElementById('subtotal').innerHTML = quantity * 1000;
-        document.getElementById('total').innerHTML = (quantity * 1000) + 50;
-    }
-    else if (quantity == 6) {
-        document.getElementById('subtotal').innerHTML = quantity * 1000;
-        document.getElementById('total').innerHTML = (quantity * 1000) + 50;
-    }
-    else if (quantity == 7) {
-        document.getElementById('subtotal').innerHTML = quantity * 1000;
-        document.getElementById('total').innerHTML = (quantity * 1000) + 50;
-    }
-    else if (quantity == 8) {
-        document.getElementById('subtotal').innerHTML = quantity * 1000;
-        document.getElementById('total').innerHTML = (quantity * 1000) + 50;
-    }
-    else if (quantity == 9) {
-        document.getElementById('subtotal').innerHTML = quantity * 1000;
-        document.getElementById('total').innerHTML = (quantity * 1000) + 50;
-    }
-    else if (quantity == 10) {
-        document.getElementById('subtotal').innerHTML = quantity * 1000;
-        document.getElementById('total').innerHTML = (quantity * 1000) + 50;
-    }
     else  {
-        window.alert("Quantity exceeds the stock number (Nanubra naka!)");
-        
+        window.alert("Quantity exceeds the stock number (Nanubra naka!)");    
+        quantity = 10;
     }
 
+    
 }
+// decrease the quantity of an item
 function descQuantity() {
+    
     quantity=quantity - 1;
     
     document.getElementById('quantity').innerHTML = quantity;
     document.getElementById('quantity1').innerHTML = quantity;
     console.log(quantity);
+
+    if (quantity <= 0) {
+        window.alert("Quantity cannot be negative or zero!");
+        quantity = 1;
+  
+
+    }else if (quantity <= 10) {
+        document.getElementById('subtotal').innerHTML = quantity * 1000;
+        document.getElementById('total').innerHTML = (quantity * 1000) + 50;
+        document.getElementById('total2').innerHTML = (quantity * 1000) + 50;
+    
+    }
+    
 }
+
